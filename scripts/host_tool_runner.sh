20ea2a7c12ccf00f7ee77bbe9cc06815aeb109df
LowkeyNEXT <LowkeyNEXT@users.noreply.github.com>
2026-08-08 17:14:25 -0500

    tools: build longitudinal MPC in host runtime

--- scripts/host_tool_runner.sh
+++ scripts/host_tool_runner.sh
@@ -282,6 +282,7 @@ ensure_host_python_extensions() {
     msgq_repo/msgq/ipc_pyx.so \
     msgq_repo/msgq/visionipc/visionipc_pyx.so \
     rednose/helpers/ekf_sym_pyx.so \
+    selfdrive/controls/lib/longitudinal_mpc_lib/c_generated_code/acados_ocp_solver_pyx.so \
     system/loggerd/bootlog \
     system/loggerd/loggerd \
     system/loggerd/encoderd
